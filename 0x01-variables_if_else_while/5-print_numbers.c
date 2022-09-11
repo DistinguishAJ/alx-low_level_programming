2a77cd13e637d6297e4ba05001b2a8b1c9f534b3
DistinguishAJ <distinguishjosiaha@gmail.com>
2022-09-11 16:35:59 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <stdlib.>
+#include <stdio.h>
 /**
- * Description main - Print out a string to stdout.
- * Return: 0 if successfull.
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successful.
  */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		printf("%d", i);
+		printf("%d%", i);
 	printf("\n");
 
 	return (0);
